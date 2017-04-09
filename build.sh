b32768aeefb0fc0e58ee5c367e1ea808b4c52b47
Bruno Luis Panuto Silva <bl.panuto@gmail.com>
2017-04-09 03:52:15 +0100

    Insert the version on the binaries
    Uses the closes tag to this particular commit, and is injected on the
    linking phase.

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@ set -e
 
 # Extract the most recent tag for this build
 VERSION=$(git describe --tags)
+
+# export the GOPATH to somewhere we can actually use the go tool on
 GOPATH=$(gb env GB_PROJECT_DIR):$(gb env GB_PROJECT_DIR)/vendor
 
 echo "building the core..."
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/autobus-core core/cmd/autobus-core
+CGO_ENABLED=0 GOOS=linux go build -ldflags "-X main.Version=$VERSION" -a -installsuffix cgo -o bin/autobus-core core/cmd/autobus-core
 echo "building the platform..."
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/autobus-platform platform/cmd/autobus-platform
+CGO_ENABLED=0 GOOS=linux go build -ldflags "-X main.Version=$VERSION" -a -installsuffix cgo -o bin/autobus-platform platform/cmd/autobus-platform
 echo "building the web API..."
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o bin/autobus-web web/cmd/autobus-web
+CGO_ENABLED=0 GOOS=linux go build -ldflags "-X main.Version=$VERSION" -a -installsuffix cgo -o bin/autobus-web web/cmd/autobus-web
