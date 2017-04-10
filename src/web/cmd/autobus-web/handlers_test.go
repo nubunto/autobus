08a18e308e9160b528a1f4487ea5477cc24de611
Bruno Luis Panuto Silva <bl.panuto@gmail.com>
2017-04-10 23:59:51 +0100

    Improved tests
    - Now, we actually *test* for implemented behaviour, e.g. created a
      line, make sure it's there;

--- src/web/cmd/autobus-web/handlers_test.go
+++ src/web/cmd/autobus-web/handlers_test.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"testing"
 
+	"gopkg.in/mgo.v2/bson"
+
 	"web"
 
 	"github.com/julienschmidt/httprouter"
@@ -30,6 +32,11 @@ func newMockedMongoBackend() *mockedMongoBackend {
 	}
 }
 
+type hooks struct {
+	beforeHandler func(b web.Backend)
+	afterHandler  func(t *testing.T, b web.Backend)
+}
+
 func TestHandlers(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
@@ -39,6 +46,7 @@ func TestHandlers(t *testing.T) {
 		registerPath string
 		query        string
 		env          *Env
+		hooks        *hooks
 		handler      func(*Env) httprouter.Handle
 		payload      io.Reader
 		assert       func(*testing.T, *httptest.ResponseRecorder)
@@ -54,6 +62,21 @@ func TestHandlers(t *testing.T) {
 				if rec.Code != http.StatusOK {
 					t.Error("should have status code 200 ok, has:", rec.Code)
 				}
+				body := rec.Body.Bytes()
+				var response struct {
+					OK      bool       `json:"ok"`
+					Message string     `json:"message"`
+					Data    []web.Line `json:"data"`
+				}
+				if err := json.Unmarshal(body, &response); err != nil {
+					t.Error("should be valid json:", rec.Body.String())
+				}
+				if !response.OK {
+					t.Error("should be a valid response:", response)
+				}
+				if len(response.Data) != 0 {
+					t.Error("should have 0 lines from mocked backend:", response.Data)
+				}
 			},
 		},
 
@@ -61,12 +84,36 @@ func TestHandlers(t *testing.T) {
 			name:         "GetLinesWithStopID",
 			method:       "GET",
 			registerPath: "/lines/:stopID",
-			requestPath:  "/lines/5bfcba74814989348fb",
+			requestPath:  "/lines/58ebed69183add0001d82019",
 			env:          newEnvWithMockedMongoBackend(),
-			handler:      handleGetLinesWithStopID,
+			hooks: &hooks{
+				beforeHandler: func(b web.Backend) {
+					b.Lines().Create(web.Line{
+						Stops: []web.StopID{
+							{ID: bson.ObjectIdHex("58ebed69183add0001d82019")},
+						},
+					})
+				},
+			},
+			handler: handleGetLinesWithStopID,
 			assert: func(t *testing.T, rec *httptest.ResponseRecorder) {
 				if rec.Code != http.StatusOK {
 					t.Error("should have status code 200 ok, has:", rec.Code)
+				}
+				body := rec.Body.Bytes()
+				var response struct {
+					OK      bool       `json:"ok"`
+					Message string     `json:"message"`
+					Data    []web.Line `json:"data"`
+				}
+				if err := json.Unmarshal(body, &response); err != nil {
+					t.Error("should be valid json:", err)
+				}
+				if !response.OK {
+					t.Error("should be a valid response:", response)
+				}
+				if len(response.Data) != 1 {
+					t.Error("should have 1 line associated with the given ID", response.Data)
 				}
 			},
 		},
@@ -94,6 +141,29 @@ func TestHandlers(t *testing.T) {
 						http.StatusCreated, http.StatusText(http.StatusCreated),
 						rec.Code, http.StatusText(rec.Code))
 				}
+				body := rec.Body.Bytes()
+				var response struct {
+					OK      bool       `json:"ok"`
+					Message string     `json:"message"`
+					Data    []web.Line `json:"data"`
+				}
+				if err := json.Unmarshal(body, &response); err != nil {
+					t.Error("should be valid json:", err)
+				}
+				if !response.OK {
+					t.Error("should be a valid response:", response)
+				}
+			},
+			hooks: &hooks{
+				afterHandler: func(t *testing.T, b web.Backend) {
+					lines, err := b.Lines().GetAll(nil)
+					if err != nil {
+						t.Error("should retrieve all lines successfully from mock")
+					}
+					if len(lines) != 1 {
+						t.Error("after handler, it should have created 1 line, created", len(lines))
+					}
+				},
 			},
 		},
 
@@ -118,6 +188,29 @@ func TestHandlers(t *testing.T) {
 						http.StatusCreated, http.StatusText(http.StatusCreated),
 						rec.Code, http.StatusText(rec.Code))
 				}
+				body := rec.Body.Bytes()
+				var response struct {
+					OK      bool          `json:"ok"`
+					Message string        `json:"message"`
+					Data    []web.BusStop `json:"data"`
+				}
+				if err := json.Unmarshal(body, &response); err != nil {
+					t.Error("should be valid json:", err)
+				}
+				if !response.OK {
+					t.Error("should be a valid response:", response)
+				}
+			},
+			hooks: &hooks{
+				afterHandler: func(t *testing.T, b web.Backend) {
+					stops, err := b.Stops().GetAll(nil)
+					if err != nil {
+						t.Error("should retrieve all stops successfully from mock")
+					}
+					if len(stops) != 1 {
+						t.Error("after handler, it should have created 1 new stop, created", len(stops))
+					}
+				},
 			},
 		},
 
@@ -135,12 +228,20 @@ func TestHandlers(t *testing.T) {
 						http.StatusOK, http.StatusText(http.StatusOK),
 						rec.Code, http.StatusText(rec.Code))
 				}
-				var response web.Response
-				if err := json.Unmarshal(rec.Body.Bytes(), &response); err != nil {
-					t.Error("response should be valid JSON, instead got error:", err)
+				body := rec.Body.Bytes()
+				var response struct {
+					OK      bool          `json:"ok"`
+					Message string        `json:"message"`
+					Data    []web.BusStop `json:"data"`
+				}
+				if err := json.Unmarshal(body, &response); err != nil {
+					t.Error("should be valid json:", err)
 				}
 				if !response.OK {
-					t.Error("response should be ok")
+					t.Error("should be a valid response:", response)
+				}
+				if len(response.Data) != 0 {
+					t.Error("should have 0 stops", response.Data)
 				}
 			},
 		},
@@ -158,12 +259,16 @@ func TestHandlers(t *testing.T) {
 						http.StatusBadRequest, http.StatusText(http.StatusBadRequest),
 						rec.Code, http.StatusText(rec.Code))
 				}
-				var response web.Response
-				if err := json.Unmarshal(rec.Body.Bytes(), &response); err != nil {
-					t.Error("response should be valid JSON, instead got error:", err)
+				body := rec.Body.Bytes()
+				var response struct {
+					OK      bool   `json:"ok"`
+					Message string `json:"message"`
+				}
+				if err := json.Unmarshal(body, &response); err != nil {
+					t.Error("should be valid json:", err)
 				}
 				if response.OK {
-					t.Error("response should be not be ok")
+					t.Error("should be a invalid response:", response)
 				}
 			},
 		},
@@ -179,7 +284,18 @@ func TestHandlers(t *testing.T) {
 			req := httptest.NewRequest(test.method, "http://example.com"+test.requestPath+"?"+test.query, test.payload)
 			w := httptest.NewRecorder()
 
+			var h hooks
+			if test.hooks != nil {
+				h = *test.hooks
+			}
+			if h.beforeHandler != nil {
+				h.beforeHandler(test.env.Backend)
+			}
 			mux.ServeHTTP(w, req)
+			if h.afterHandler != nil {
+				h.afterHandler(tt, test.env.Backend)
+			}
+
 			test.assert(tt, w)
 		})
 	}
